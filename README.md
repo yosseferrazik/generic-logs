74955face128f69be68b3967b2c6063b329cc2f5
Yossef FTW <82938832+yosseferrazik@users.noreply.github.com>
2023-05-13 20:38:19 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Generic Logs
 
 
-Generic-logs es un paquete para NodeJS que mejora ligeramente la funcion `console.log` haciendola algo mas visual y intiutiva ofreciendo 4 diferentes funciones 
-
+Generic-logs es un paquete para NodeJS que mejora ligeramente la funcion `console.log` haciendola algo mas visual y intiutiva ofreciendo varias funciones 
+![](https://i.imgur.com/skpqoRy.png)
 ## Antes de empezar
 ### **Instalar el paquete** 
 
@@ -11,17 +11,127 @@ npm install generic-logs@latest --save
 ```
 ### **Importarlo** 
 
-```
-const log = require('generic-logs')
+```js
+const log = require('generic-logs');
 ```
 ### **Ejemplo**
-```
-log.info("Esto es un mensaje informativo")
-```
->![xd](https://i.imgur.com/bM2yRt9.png)
+```js
+const log = require('generic-logs');
 
-# Uso
+log.info("Esto es un mensaje informativo");
 ```
+>![xd](https://i.imgur.com/RGQyfeP.png)
+
+# Documentacion
+El paquete consta actualmente de 7 funciones nuevas para mejorar visualmente tu logs.
+- `warn()`
+- `info()`
+- `debug()`
+- `error()`
+- `success()`
+- `startLoader()` y `stopLoader()`
+- `separator()`
+
+## Warn `.warn(texto)`
+```js
+// Uso basico
+
+log.warn(mensaje)
 
 
+// Loggear multiples objetos
+log.warn(obj1, obj2, obj3, obj4...)
 ```
+## Info `.info(texto)`
+```js
+// Uso basico
+
+log.info(mensaje)
+
+
+// Loggear multiples objetos
+log.info(obj1, obj2, obj3, obj4...)
+```
+## Error `.error(texto)`
+```js
+// Uso basico
+
+log.error(mensaje)
+
+
+// Loggear multiples objetos
+log.error(obj1, obj2, obj3, obj4...)
+```
+## Debug `.debug(texto)`
+```js
+// Uso basico
+
+log.warn(mensaje)
+
+
+// Loggear multiples objetos
+log.warn(obj1, obj2, obj3, obj4...)
+```
+
+## Success `.success(texto)`
+```js
+// Uso basico
+
+log.success(mensaje)
+
+
+// Loggear multiples objetos
+log.success(obj1, obj2, obj3, obj4...)
+```
+## Separador `.separator(color,estilo)`
+*Es importante que este en formato de string*
+### Colores (por defecto es azul)
+- Rojo : `"red"`
+- Azul : `"blue"`
+- Cyan : `"cyan"`
+- Magenta : `"magenta"`
+- Amarillo : `"yellow"`
+- Negro : `"black"`
+- Verde : `"green"`
+- Blanco : `"white"`
+### Estilos (por defecto son quadrados)
+- Linea : `"line"`
+- Blanco&Negro : `"chess"`
+- Guión : `"script"`
+- Triangulos : `"triangles"`
+- Cadenas : `"chain"`
+
+
+```js
+// Estilos
+
+log.separator("","triangles")
+// △▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△
+
+log.separator("","script")
+// ----------------------------------
+
+log.separator("","line")
+// ┣━━━━━━━━━━━━━━━━━━━━┫
+
+log.separator("","chess")
+// ▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚
+
+log.separator("","chain")
+//  ⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘ 
+
+log.separator()
+// ▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅
+```
+
+## Loading `.startLoader(texto)` y `.stopLoader()`
+Con `.startLoader` inicias un "bucle" en el que sale un mensaje y con `.stopLoader()` frenas el bucle . 
+```js
+// inicias
+log.startLoader('Hola')
+
+// acabas
+log.stopLoader()
+```
+![](https://i.imgur.com/kcAjRzF.gif)
+
--- index.js
+++ index.js
@@ -1,20 +1,208 @@
-const logger = require('node-color-log');
-let date = new Date()
-const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
-const actualhour = "["+hour+":"+minutes+"]";
+const date = new Date();
+const [hour, minutes, seconds] = [
+  date.getHours(),
+  date.getMinutes(),
+  date.getSeconds(),
+];
+const actualhour = "[" + hour + ":" + minutes + "]";
+const style = {
+  reset: "\x1b[0m",
+  bold: "\x1b[1m",
+  dim: "\x1b[2m",
+  italic: "\x1b[3m",
+  underscore: "\x1b[4m",
+  reverse: "\x1b[7m",
+  strikethrough: "\x1b[9m",
+  backoneline: "\x1b[1A",
+  cleanthisline: "\x1b[K",
+};
+const bg = {
+  black: "\x1b[40m",
+  red: "\x1b[41m",
+  green: "\x1b[42m",
+  yellow: "\x1b[43m",
+  blue: "\x1b[44m",
+  magenta: "\x1b[45m",
+  cyan: "\x1b[46m",
+  white: "\x1b[47m",
+};
+const color = {
+  black: "\x1b[30m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  magenta: "\x1b[35m",
+  cyan: "\x1b[36m",
+  white: "\x1b[37m",
+};
+const P = [
+  "\\ㅤLOADING.  ㅤ〉ㅤ",
+  "|ㅤLOADING.. ㅤ〉ㅤ",
+  "/ㅤLOADING...ㅤ〉ㅤ",
+  "-ㅤLOADING   ㅤ〉ㅤ",
+];
+let x = 0;
+let loaderInterval;
 
-exports.error = function(message) {
-    return logger.color('red').bold().log(`${actualhour}ㅤERRORㅤ🡆ㅤ${message}`);
+exports.error = function (text) {
+  return console.log(
+    "\n" +
+      actualhour +
+      color.red +
+      style.bold +
+      "ㅤERRORㅤ〉ㅤ" +
+      style.reset +
+      color.red +
+      `${text}`
+      + style.reset
+  );
+};
+exports.success = function (text) {
+  return console.log(
+    "\n" +
+      actualhour +
+      color.green +
+      style.bold +
+      "ㅤSUCCESSㅤ〉ㅤ" +
+      style.reset +
+      color.green +
+      `${text}`
+      + style.reset
+  );
+};
+exports.info = function (text) {
+  return console.log(
+    "\n" +
+      actualhour +
+      color.blue +
+      style.bold +
+      "ㅤINFOㅤ〉ㅤ" +
+      style.reset +
+      color.blue +
+      `${text}`
+      + style.reset
+  );
+};
+exports.warn = function (text) {
+  return console.log(
+    "\n" +
+      actualhour +
+      color.yellow +
+      style.bold +
+      "ㅤWARNINGㅤ〉ㅤ" +
+      style.reset +
+      color.yellow +
+      `${text}`
+      + style.reset
+  );
+};
+exports.debug = function (text) {
+  return console.log(
+    "\n" +
+      actualhour +
+      color.cyan +
+      style.bold +
+      "ㅤDEBUGㅤ〉ㅤ" +
+      style.reset +
+      color.cyan +
+      `${text}`
+      + style.reset
+  );
+};
+exports.startLoader = function (text) {
+  loaderInterval = setInterval(() => {
+    process.stdout.write(
+      color.magenta +
+        style.bold +
+        `\r${P[x++]}` +
+        style.reset +
+        color.magenta +
+        `${text}`
+        + style.reset
+    );
+    x %= P.length;
+  }, 250);
+};
+exports.stopLoader = function () {
+  clearInterval(loaderInterval);
+};
+exports.separator = function (textcolor, type) {
+  let finalcolor;
+  switch (textcolor) {
+    case "red":
+      finalcolor = color.red;
+      break;
+    case "blue":
+      finalcolor = color.blue;
+      break;
+    case "cyan":
+      finalcolor = color.cyan;
+      break;
+    case "yellow":
+      finalcolor = color.yellow;
+      break;
+    case "magenta":
+      finalcolor = color.magenta;
+      break;
+    case "green":
+      finalcolor = color.green;
+      break;
+    case "white":
+      finalcolor = color.white;
+      break;
+    case "black":
+      finalcolor = color.black;
+      break;
+    default:
+      finalcolor = color.blue;
+  }
+  switch (type) {
+    case "chess":
+      console.log(
+        finalcolor +
+          `▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚▚` +
+          style.reset
+      );
+      break;
+    case "triangles":
+      console.log(
+        finalcolor +
+          style.bold +
+          "△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽△▽" +
+          style.reset
+      );
+      break;
+    case "line":
+      console.log(
+        finalcolor +
+          "┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫" +
+          style.reset
+      );
+      break;
+    case "script":
+      console.log(
+        finalcolor +
+          style.bold +
+          "------------------------------------------------------------------------------" +
+          style.reset
+      );
+      break;
+    case "chain":
+      console.log(
+        finalcolor +
+          style.bold +
+          "⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ ⫘ " +
+          style.reset
+      );
+      break;
+    default:
+      console.log(
+        finalcolor +
+          "▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅" +
+          style.reset
+      );
+  }
+}
 
-}
-exports.debug = function(message) {
-    return logger.color('cyan').bold().log(`${actualhour}ㅤDEBUGㅤ🡆ㅤ${message}`);
 
-}
-exports.info = function(message) {
-    return logger.color('blue').bold().log(`${actualhour}ㅤINFOㅤ🡆ㅤ${message}`);
-
-}
-exports.succes = function(message) {
-    return logger.color('green').bold().log(`${actualhour}ㅤSUCCESㅤ🡆ㅤ${message}`);
-}
