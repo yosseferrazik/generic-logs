3d71b5aed9692c1680bf1034e8ac2198042bfef0
Yossef FTW <yosseferrazik@gmail.com>
2023-05-17 23:31:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,85 @@
 // "ㅤ"
 var date1 = new Date;
-var minutes = date1.getMinutes();
-var hour = date1.getHours();
-
-const actual = `${hour}:${minutes}`
-
-const style = {
-  reset: "\x1b[0m",
-  bold: "\x1b[1m",
-  dim: "\x1b[2m",
-  italic: "\x1b[3m",
-  underscore: "\x1b[4m",
-  reverse: "\x1b[7m",
-  strikethrough: "\x1b[9m",
-  backoneline: "\x1b[1A",
-  cleanthisline: "\x1b[K",
+const actual = `${date1.getHours()}:${date1.getMinutes()}`
+const info = (message = '') => {
+    return console.log(`[${actual}] \x1b[34m INFO 〉${message}\x1b[0m`);
 };
-
-const bg = {
-  black: "\x1b[40m",
-  red: "\x1b[41m",
-  green: "\x1b[42m",
-  yellow: "\x1b[43m",
-  blue: "\x1b[44m",
-  magenta: "\x1b[45m",
-  cyan: "\x1b[46m",
-  white: "\x1b[47m",
+const success = (message = '') => {
+    return console.log(`[${actual}] \x1b[32m SUCCESS 〉${message}\x1b[0m`);
 };
-
-const color = {
-  black: "\x1b[30m",
-  red: "\x1b[31m",
-  green: "\x1b[32m",
-  yellow: "\x1b[33m",
-  blue: "\x1b[34m",
-  magenta: "\x1b[35m",
-  cyan: "\x1b[36m",
-  white: "\x1b[37m",
+const warning = (message = '') => {
+    return console.log(`[${actual}] \x1b[33m WARNING 〉${message}\x1b[0m`);
 };
-
-
-
-const applyStyles = (text, styles) => {
-  const combinedStyles = styles.join('');
-  return `${combinedStyles}${text}\x1b[0m`;
+const error = (message = '') => {
+    return console.log(`[${actual}] \x1b[31m ERROR 〉${message}\x1b[0m`);
 };
+const custom = ({ title = '', message = '', color = '' }) => {
+    let setcolor;
+    switch (color) {
+        case 'black':
+            setcolor = "\x1b[30m";
+            break;
+        case 'red':
+            setcolor = "\x1b[31m";
+            break;
+        case 'green':
+            setcolor = "\x1b[32m";
+            break;
+        case 'yellow':
+            setcolor = "\x1b[33m";
+            break;
+        case 'blue':
+            setcolor = "\x1b[34m";
+            break;
+        case 'magenta':
+            setcolor = "\x1b[35m";
+            break;
+        case 'cyan':
+            setcolor = "\x1b[36m";
+            break;
+        case 'white':
+            setcolor = "\x1b[37m";
+            break;
+    }
 
-const log = ({ text, styles }) => {
-  const formattedText = applyStyles(text, styles);
-  console.log(`[${actual}] ${formattedText}${style.reset}`);
+    return console.log(`[${actual}] ${setcolor} ${title} 〉${message} \x1b[0m`)
+
 };
-
-const info = (message) => {
-  log({ text: `INFO 〉 ${message}`, styles: [color.blue] });
-};
-
-const success = (message) => {
-  log({ text: `SUCCESS 〉 ${message}`, styles: [color.green] });
-};
-
-const warning = (message) => {
-  log({ text: `WARNING 〉 ${message}`, styles: [color.yellow] });
-};
-
-const error = (message) => {
-  log({ text: `ERROR 〉 ${message}`, styles: [color.red] });
-};
-
 const separator = (char = '-', length = 10) => {
-  const separatorText = char.repeat(length);
-  log({ text: separatorText, styles: [] });
+    const separatorText = char.repeat(length);
+    return console.log(separatorText);
 };
 
 
+const reset = (message) => `\x1b[0m${message}\x1b[0m`;
+const bold = (message) => `\x1b[1m${message}\x1b[0m`;
+const dim = (message) => `\x1b[2m${message}\x1b[0m`;
+const italic = (message) => `\x1b[3m${message}\x1b[0m`;
+const underscore = (message) => `\x1b[4m${message}\x1b[0m`;
+const reverse = (message) => `\x1b[7m${message}\x1b[0m`;
+const strikethrough = (message) => `\x1b[9m${message}\x1b[0m`;
+const backoneline = (message) => `\x1b[1A${message}\x1b[0m`;
+const cleanthisline = (message) => `\x1b[K${message}\x1b[0m`;
+const black = (message) => `\x1b[30m${message}\x1b[0m`;
+const red = (message) => `\x1b[31m${message}\x1b[0m`;
+const green = (message) => `\x1b[32m${message}\x1b[0m`;
+const yellow = (message) => `\x1b[33m${message}\x1b[0m`;
+const blue = (message) => `\x1b[34m${message}\x1b[0m`;
+const magenta = (message) => `\x1b[35m${message}\x1b[0m`;
+const cyan = (message) => `\x1b[36m${message}\x1b[0m`;
+const white = (message) => `\x1b[37m${message}\x1b[0m`;
+const bgblack = (message) => `\x1b[40m${message}\x1b[0m`;
+const bgred = (message) => `\x1b[41m${message}\x1b[0m`;
+const bggreen = (message) => `\x1b[42m${message}\x1b[0m`;
+const bgyellow = (message) => `\x1b[43m${message}\x1b[0m`;
+const bgblue = (message) => `\x1b[44m${message}\x1b[0m`;
+const bgmagenta = (message) => `\x1b[45m${message}\x1b[0m`;
+const bgcyan = (message) => `\x1b[46m${message}\x1b[0m`;
+const bgwhite = (message) => `\x1b[47m${message}\x1b[0m`;
 
 
-module.exports = { log, info, success, warning, error, separator, color, style, bg };
+
+module.exports = { custom, info, success, warning, error, separator, red, blue, magenta, white, black, green, yellow, cyan, bgred, bgblue, bgmagenta, bgwhite, bgblack, bggreen, bgyellow, bgcyan, reset, bold, dim, italic, reverse, underscore, strikethrough, backoneline, cleanthisline };
+
+
 
